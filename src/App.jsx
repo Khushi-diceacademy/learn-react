679b37c67cee48905107c12ee5333bc3e2c29dfa
Khushi-diceacademy <khushi@dice-academy.com>
2024-12-16 15:28:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function App(){
+  return <h1>heLlo world</h1>
+}
+export default App
+
+// jsx -> javascript + XML extensible markup language ye html se milti jhulti language h lekin ye html ni h pheli bat html m 2+2 = 2+2 lekin jsx - 2+2 = 4 (2+2) issliye jsx jada powerful h toh hum ab se jsx pe kaam karege ye naming ko bhuat follow karta toh humri files ka naam soch samj ke banana captical case camel case follow karna dusri chej jab kisi folder ka naam kisi or naam se h or tum function kisi or naam se bana rhe ho that is called bad practice other thing don't mess with the folder structure 
